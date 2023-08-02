6413f83e8dc4910fd34d586937421ccb3f5aae97
darkclip <darkclip@users.noreply.github.com>
2023-08-03 03:12:22 +0800

    update

--- cf-watch.sh
+++ cf-watch.sh
@@ -6,7 +6,7 @@ get_rnd(){
 };
 
 get_ping_loss(){
-    loss_percent=$(ping -c 5 -q $1 | awk '/packet loss/{print $((NF-4))}');
+    loss_percent=$(ping -c 5 -q $1 | awk '/packet loss/{print $7}');
     loss=${loss_percent%?};
     echo ${loss%.*};
     return $?;
