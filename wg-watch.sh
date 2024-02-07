89c60e89cbca485bd228df7e714c352753f4fd23
darkclip <darkclip@users.noreply.github.com>
2024-02-07 22:59:02 +0800

    update

--- wg-watch.sh
+++ wg-watch.sh
@@ -112,8 +112,8 @@ generate_ips(){
 speedtest(){
     exe_file='/var/tmp/warp';
     result_file='/var/tmp/result.csv'
-    if [ ! -x "$exe_file" ]; then
-        curl -SfLo "$exe_file" "https://git.tink.ltd:8443/netlist.git/tree/warp-$1?raw=true&h=better-warp-ip" 2>&1;
+    if [ ! -x "$exe_file" ]; thens
+        curl -SfLo "$exe_file" "https://git.tink.ltd:8443/tink/netlist/raw/branch/better-warp-ip/warp-$1" 2>&1;
     fi;
     ulimit -n 102400;
     chmod +x "$exe_file" && "$exe_file" -file "$2" -output "$result_file" >/dev/null 2>&1;
